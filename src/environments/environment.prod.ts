5b5eb3cd466ebfd996ddb41afd8aede406b8b6ea
Dawid Marcinków <dawid.marcinkow@gmail.com>
2023-09-16 07:34:32 +0200

    New devims

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'https://54.37.137.112:8443/',
-  // baseURL: 'https://devims.pl:8443/',
+  baseURL: 'https://devims.pl:8443/',
   currentSeasonId: 26 // 2023/2024
 };
