a1c985837988ebc433280e16fd61657bab66244d
Dawid Marcinków <dawid.marcinkow@gmail.com>
2023-09-18 15:48:52 +0000

    Revert "Survey without esor"
    This reverts commit 506605d5a3ff8463cd3997d9c9cc269117dbfe3d

--- src/app/esor/pre-season-survey/pre-season-survey.component.ts
+++ src/app/esor/pre-season-survey/pre-season-survey.component.ts
@@ -86,7 +86,6 @@ export class PreSeasonSurveyComponent implements OnInit {
   }
 
   getSurveyByPw() {
-    this.isLoading = true;
     this.surveyByPwNotFound = false;
     this.http.get<any>(this.baseUrl + 'esor/pre-season-survey/' + this.surveyPassword).subscribe({
       next: (response) => {
